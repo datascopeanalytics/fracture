64d597daf21982bea2c93f21c6877fddcf36f329
stringertheory <mike.stringer@datascopeanalytics.com>
2014-05-19 17:27:37 -0500

    Hey @bjlange, it's drawing! Not correctly, though. I think there is something with how we're passing around vertices.

--- fracture.js
+++ fracture.js
@@ -1,7 +1,7 @@
 var elem = document.getElementById('one');
 
-var width = 600;
-var height = 400;
+var width = 400;
+var height = 300;
 var two = new Two({
   width: width,
   height: height,
@@ -16,10 +16,11 @@ var lower_right = new Two.Anchor(two.width, two.height);
 var box = two.makePolygon([upper_left, upper_right, lower_right, lower_left]);
 box.fill = 'rgba(255, 0, 0, 0.5)';
 box.stroke = 'rgba(255, 0, 0, 1.0)';
-box.linewidth = 0;
+box.linewidth = 2;
 
 var find_center = function (polygon) {
   var bounding_rect = polygon.getBoundingClientRect(true);
+  console.log('bounding rect:', bounding_rect);
   return new Two.Anchor(
     bounding_rect.left + bounding_rect.width / 2,
     bounding_rect.top + bounding_rect.height / 2
@@ -28,23 +29,68 @@ var find_center = function (polygon) {
 
 var iter_pairs = function (array) {
   var result = [];
-  for (var i=0; i <= array.length; i++) {
+  for (var i=0; i < array.length; i++) {
     result.push([array[i], array[(i + 1) % array.length]]);
   }
   return result;
 };
 
-var find_intercept(vertex_a, vertex_b, center, angle) {
+var find_intercept = function (vertex_a, vertex_b, center, angle) {
   // vertexes and center are anchors. angle is the angle of the
   // subdividing line in radians. returns null if lines are parallel,
   // and returns an anchor with intercept otherwise.
-  return null;
+
+  // huzzah for wikipedia!
+  // http://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+  var x1 = vertex_a.x;
+  var y1 = vertex_a.y;
+  var x2 = vertex_b.x;
+  var y2 = vertex_b.y;
+  var x3 = center.x;
+  var y3 = center.y;
+  var hack_radius = 99999999999; // .99.99999.99999.999.999
+  var x4 = center.x + hack_radius * Math.cos(angle);
+  var y4 = center.y + hack_radius * Math.sin(angle);
+  var denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+  if (denominator !== undefined) {
+    var a = (x1 * y2 - y1 * x2);
+    var b = (x3 * y4 - y3 * x4);
+    var n_x = a * (x3 - x4) - (x1 - x2) * b;
+    var n_y = a * (y3 - y4) - (y1 - y2) * b;
+    return new Two.Anchor(n_x / denominator, n_y / denominator);
+  }
+  else {
+    return null;
+  }
 };
 
-var angle_from_origin(vertex, center) {
+var angle_from_origin = function (vertex, center) {
   // return angle in radians between the "zero horizontal" and the ray
-  // defined by vertex and center.
-  return null;
+  // defined by vertex and center. atan2 returns angle between -pi and
+  // pi, and we want the angle between 0 and 2*pi, so if it's
+  // negative, add two pi.
+  var result = Math.atan2(vertex.y, vertex.x);
+  if (result < 0) {
+    return 2 * Math.PI + result;
+  }
+  else {
+    return result;
+  }
+}
+
+var close_enough = function (angle, lower_bound, upper_bound) {
+  // the following:
+  // intercept_angle >= angle && intercept_angle < next_angle;
+  // is running into floating point errors.
+  // This does the same thing, but to lower precision on the lower
+  // bound side.
+  if (Math.abs(angle - lower_bound) < 0.000001) {
+    return true;
+  }
+  else {
+    return (angle >= lower_bound && angle < upper_bound);
+  }
+
 }
 
 var sub_polygons = function (polygon, n) {
@@ -52,6 +98,7 @@ var sub_polygons = function (polygon, n) {
 
   // find center of parent polygon
   var center = find_center(polygon);
+  console.log('center', center.x, center.y);
 
   // get angles of subdividing lines
   var angles = _.map(_.range(n), function (i) {
@@ -75,7 +122,7 @@ var sub_polygons = function (polygon, n) {
     var good_intercept = _.find(side_intercepts, function (intercept) {      
       if (intercept !== null) {
 	var intercept_angle = angle_from_origin(intercept, center);
-	return (intercept_angle >= angle && intercept_angle < next_angle);
+	return close_enough(intercept_angle, angle, next_angle);
       } 
       else {
 	return false;
@@ -97,13 +144,18 @@ var sub_polygons = function (polygon, n) {
       }
     });
     polylist.push(new_vertices[(index+1) % new_vertices.length]);
-    result.push(two.makePolygon(polylist));
+    console.log('poly', polylist);
+    var yeah = two.makePolygon(polylist);
+    yeah.stroke = 'black';
+    yeah.linewidth = 2;
+    yeah.fill = 'white';
+    result.push(yeah);
   });
 
   return result;
 }
 
-sub_polygons(box, 3);
+sub_polygons(box, 7);
 two.update();
 
 // two.bind('update', function(frameCount) {
